45f58c4bd727be164b49afb133693c93432f8218
axszdwse <a0284659570@gmail.com>
2019-03-04 18:55:45 +0800

    Initial commit

---
+++ src/main/kotlin/com/michael/Hello.kt
@@ -0,0 +1,13 @@
+package com.michael
+
+fun main(args: Array<String>) {
+    //println("Hello World");
+    //Human().hello();
+    val h=Human();
+    h.hello();
+}
+class Human{
+    fun hello(){
+        println("Hello KT");
+    }
+}
