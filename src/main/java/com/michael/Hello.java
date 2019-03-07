27c627447b4f6eaa4fb35051b5d4e7aeaca0c5d7
axszdwse <a0284659570@gmail.com>
2019-03-08 02:18:06 +0800

    Java and KT variable,date type

--- src/main/java/com/michael/Hello.java
+++ src/main/java/com/michael/Hello.java
@@ -5,5 +5,17 @@ public class Hello {
         //System.out.println("Hello World");
         Person p=new Person();
         p.hello();
+        int age=22;
+        Integer age2=22;
+        char c='我';
+        Character c2='A';
+        byte b=120;
+        float weight=102.2f;
+        Float weight2=102.2f;
+        double height=1.75;
+        boolean adult=true;
+        boolean school=false;
+        String name="Michael";
+
     }
 }
--- src/main/kotlin/com/michael/Hello.kt
+++ src/main/kotlin/com/michael/Hello.kt
@@ -5,7 +5,13 @@ fun main(args: Array<String>) {
     //Human().hello();
     val h=Human();
     h.hello();
+    val age=22;
+    var weight=102.2f;
+    val name="Michael";
+    var name1 : String;
+    name1="Michael.Wang";
 }
+
 class Human{
     fun hello(){
         println("Hello KT");
